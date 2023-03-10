84a57fc2863cae8f4fd318ffeb84462cf653f9de
Rap1zer <125188052+Rap1zer@users.noreply.github.com>
2023-03-10 17:19:07 +0000

    Update cardStyling.js

--- cardStyling.js
+++ cardStyling.js
@@ -5,17 +5,21 @@ function addCard() {
    
       gridContainerEl.appendChild(card);
       cardCount++;
-      
-      updateCardStyleSheet();
+
+      updateCardGrid();
    }
 }
 
-function updateCardStyleSheet() {
-   let cards = document.getElementsByClassName("card");
+window.addEventListener("resize", updateCardGrid);
+
+function updateCardGrid() {
+   const cards = document.getElementsByClassName("card");
    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
    // This variable stores the number of cards the view width can fit
    let numOfColumns = vw / (300 + 20 * 2);
+   console.log(vw + " / 300 + 40 = " + numOfColumns);
    for (let i = 0; i < cards.length; i++) {
-      cards[i].style.maxWidth = vw / numOfColumns;
+      cards[i].style.maxWidth = vw / numOfColumns + "px";
+      console.log(vw + " / " + numOfColumns + " = " + cards[i].style.maxWidth);
    }
 }
