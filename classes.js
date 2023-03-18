eed6bbb64b18843b1713c4ad762689fba0cda906
Rap1zer <125188052+Rap1zer@users.noreply.github.com>
2023-03-18 13:57:04 +0000

    createdclose button for cards

--- classes.js
+++ classes.js
@@ -8,11 +8,17 @@ class Suggestion {
 }
 
 class Card {
-   constructor(el, name, lat, lon, title, temp, description, humidity, wind) {
+   constructor(el, name, lat, lon, title, temp, description, humidity, wind, deleteBtn) {
       this.el = el;
       this.name = name;
       this.lat = lat;
       this.lon = lon;
+      this.title = title;
+      this.tmep = temp;
+      this.description = description;
+      this.humidity = humidity;
+      this.wind = wind;
+      this.deleteBtn = deleteBtn;
    }
 
    addHTMLmarkup() {
@@ -20,6 +26,9 @@ class Card {
       cardContainer.classList.add("card-container");
       const divTop = document.createElement("div");
       const divBottom = document.createElement("div");
+      this.deleteBtn = document.createElement("div");
+      this.deleteBtn.classList.add("close-btn");
+      this.deleteBtn.addEventListener("click", this.closeCard);
       this.temp = document.createElement("h2");
       this.title = document.createElement("h3");
       this.title.textContent = this.name;
@@ -28,7 +37,21 @@ class Card {
       this.humidity = document.createElement("h4");
       divTop.append(this.temp, this.description);
       divBottom.append(this.wind, this.humidity, this.title);
-      cardContainer.append(divTop, divBottom);
+      cardContainer.append(this.deleteBtn, divTop, divBottom);
       this.el.append(cardContainer);
+   }
+
+   closeCard() {
+      const card = this.parentElement.parentElement;
+
+      for (let i = 0; i < cards.length; i++) {
+         if (cards[i].el === card) {
+            cards.splice(i, 1);
+         }
+      }
+
+      cardCount--;
+      updateCardSizing();
+      card.remove();
    }
 }
